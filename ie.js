abbc8af76a12ebbb3abb4078a731e2691bca9474
ditto7890 <kwak071219@gmail.com>
2020-05-08 21:32:59 +0900

    update

--- ie.js
+++
@@ -1,4 +0,0 @@
-if (!!document.documentMode || eval('let a=()=>{};') instanceof EvalError) {
-    document.body.removeChild(document.querySelector('.main'))
-    document.write('<h1>Internet Explorer 는 지원하지 않습니다.<br>Chrome, Firefox 같은 브라우저를 이용해 주세요.</h1>')
-}
