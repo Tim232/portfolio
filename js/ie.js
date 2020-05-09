66df5aebcd00158d188cb0b6a81f5d3c0f6e3021
ditto7890 <kwak071219@gmail.com>
2020-05-09 14:30:05 +0900

    update

--- js/ie.js
+++ js/ie.js
@@ -1,4 +1,6 @@
-if (!!document.documentMode || eval('let a=()=>{};') instanceof EvalError) {
+var isIE = /*@cc_on!@*/false || !!document.documentMode
+
+if (isIE) {
     document.body.removeChild(document.querySelector('.main'))
-    document.write('<style>body{display:flex;align-items:center;justify-content:center;flex-direction:column;}</style><h1>Internet Explorer 는 지원하지 않습니다.<br>Chrome, Firefox 같은 브라우저를 이용해 주세요.</h1>')
+    document.write('<style>.main{display:flex;align-items:center;justify-content:center;flex-direction:column;}</style><div class="main"><h1>Internet Explorer 는 지원하지 않습니다.<br>Chrome, Firefox, Naver Whale 같은 브라우저를 이용해 주세요.</h1></div>')
 }
