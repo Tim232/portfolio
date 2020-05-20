3f7b7155a3ab319be50aff17f3009e2d69f7a95c
ditto7890 <kwak071219@gmail.com>
2020-05-20 23:33:15 +0900

    update

--- js/index.js
+++ js/index.js
@@ -1,6 +1,2 @@
-document.addEventListener('dragstart', e => e.preventDefault())
-document.addEventListener('contextmenu', e => e.preventDefault())
-document.addEventListener('selectstart', e => e.preventDefault())
-document.addEventListener('keydown', e => {
-    if ((e.keyCode === 123) || (e.ctrlKey && e.shiftKey && e.keyCode === 73)) e.preventDefault()
-})
+/*document.addEventListener('dragstart', e => e.preventDefault())
+document.addEventListener('selectstart', e => e.preventDefault())*/
