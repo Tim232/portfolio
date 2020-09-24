a24dcc7ff6a9ae4dec0ec1fe65801bce40140ef6
ditto7890 <ditto7891@gmail.com>
2020-09-24 22:59:05 +0900

    Update

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 document.body.addEventListener('dragstart', event => event.preventDefault())
+
+new fullpage('#fullpage', {
+    licenseKey: 'asdf',
+    navigation: true,
+    navigationTooltips: ['Main', 'Info', 'Projects'],
+    anchors: ['1', '2', '3'],
+    scrollingSpeed: 1000
+})
